eaf00b9773232d6355e23f0065556da7e34fc4ae
Alexandru Grigoruță <alexandrugr@yonder.local>
2021-12-15 17:42:08 +0200

    3-routes-and-api

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-## 2-project-config
+## 3-routes-and-api
 
-1.  Checkout to new branch
-    
-2.  It should have all the needed dependencies in a package.json, along with the boilerplate code
-    
-3.  Import it in vue cli ui
-    
-4.  Npm install and npm serve
+1.  Go to server folder
+
+2.  Npm install
+
+3.  Install and configure mongoDB
+
+4.  Run the seeder
+
+5.  Check the documents using compass
+
+6.  Configure the 3 routes from the boilerplate -> /, /about, /seating, and render their associated components
+
+7.  Go to /about, see that you got a message that you’re not connected to the API
+
+8.  Make it so when you render the component, you make a call towards the /test endpoint
+
+9.  See what happens
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="nav">
+      <a>Home</a> |
+      <a>About</a> |
+      <a>Seating</a>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+</style>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
